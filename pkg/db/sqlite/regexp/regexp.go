2fc22b5e5a1a6cf7e4a48c2b7b951d091146288d
David Stotijn <dstotijn@gmail.com>
2020-10-25 15:19:05 +0100

    Fix cross compilation to work with custom regexp function

--- pkg/db/sqlite/regexp/regexp.go
+++ pkg/db/sqlite/regexp/regexp.go
@@ -1,6 +1,10 @@
 package regexp
 
+// #ifndef USE_LIBSQLITE3
+// #include <sqlite3-binding.h>
+// #else
 // #include <sqlite3.h>
+// #endif
 //
 // // Extension function defined in regexp.c.
 // extern int sqlite3_regexp_init(sqlite3*, char**, const sqlite3_api_routines*);
